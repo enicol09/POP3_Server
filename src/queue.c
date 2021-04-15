8640ab23c1fefeadd1aeced8df712f342b5c20ce
pmikel01 <49363815+pmikel01@users.noreply.github.com>
2021-04-15 18:43:37 +0300

    Update queue.c

--- src/queue.c
+++ src/queue.c
@@ -3,6 +3,15 @@
 extern request *front = NULL;
 extern request *rear = NULL;
 
+ /**
+ * @brief This function pushes a job in the queue.
+ *
+ * We call enqueue in order to push a job in the tail of the queue.
+ *
+ * @param int *client file descriptor of each client
+ * @param struct sockaddr_in *client_ptr socket of each client
+ * @return void
+ * */
 void enqueue(int *client, struct sockaddr_in *client_ptr) {
     request *r = malloc(sizeof(request));
     r->socket = client;
@@ -16,6 +25,14 @@ void enqueue(int *client, struct sockaddr_in *client_ptr) {
     rear = r;
 }
 
+  /**
+ * @brief This function unqueues a job from the queue.
+ *
+ * We call edequeue in order to pull a job from the head of the queue.(And update the new head to the next job)
+ *
+ * @param request *r pointer to return socket and file descriptor
+ * @return int -1 if unqueue failed
+ * */
 int dequeue(request *r) {
     if (front==NULL) {
         return -1;
